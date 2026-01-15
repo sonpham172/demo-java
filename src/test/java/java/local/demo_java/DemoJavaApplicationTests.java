2f5857cad23322d1602d2d9b91a165463f72d8c7
Finn Pham <finn.pham@1bit.com.ph>
2026-01-15 22:33:01 +0700

    init

---
+++ src/test/java/java/local/demo_java/DemoJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package java.local.demo_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
